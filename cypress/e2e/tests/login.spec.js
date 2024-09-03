e48e71a3e2d2333380d06cfe2961eac5853b8bac
Kapil <kapil9713@gmail.com>
2024-09-03 20:05:55 +0530

    First Commit-Cypress Project

---
+++ cypress/e2e/tests/login.spec.js
@@ -0,0 +1,13 @@
+describe('Login Test', () => {
+    it('Should log in successfully with valid credentials', () => {
+      // Visit the login page
+      cy.visit('https://www.google.com');
+  
+      // Get and print the page title
+      cy.title().then((title) => {
+        cy.log('Page title is: ' + title);
+        console.log('Page title is:', title); // This will log in the console where Cypress runs
+      });
+    });
+  });
+  
